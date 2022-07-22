54e11f53848847c17b006d94d126dcef01f1db66
Luiz <luizsaulo@yahoo.com.br>
2022-07-22 17:53:16 -0300

    adding style to the start countdown button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator, StartCountdownButton } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 
 export function Home() {
@@ -8,10 +8,17 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput 
+            id="task" 
+            placeholder="Dê um nome para seu projeto" 
+          />
 
           <label htmlFor="">durante</label>
-          <input type='number' id='minutesAmount' />
+          <MinutesAmountInput 
+            type='number' 
+            id='minutesAmount' 
+            placeholder="00"
+          />
 
           <span>minutos.</span>
         </FormContainer>     
