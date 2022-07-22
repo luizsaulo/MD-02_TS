12636b70eb7fa078761b747caf9ce69120cb41a9
Luiz <luizsaulo@yahoo.com.br>
2022-07-22 17:37:05 -0300

    working on the start countdown button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, Separator, StartCountdownButton } from "./styles";
 
 
 export function Home() {
@@ -24,10 +24,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -53,3 +53,33 @@ export const Separator = styled.div`
     display: flex;
     justify-content: center;
 `;
+
+export const StartCountdownButton = styled.button`
+    width: 100%;
+    padding: 1rem;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    background: ${(props) => props.theme['green-500']};
+    color: ${(props) => props.theme['gray-100']};
+    
+    border: 0;
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+    }
+
+`;
