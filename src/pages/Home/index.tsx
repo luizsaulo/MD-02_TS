cefc6a6c43714fca0d5bc134db804fc3998f9d7f
Luiz <luizsaulo@yahoo.com.br>
2022-07-25 15:07:01 -0300

    working on the form adding logics to it

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,28 @@
 import { Play } from "phosphor-react";
+import { useForm } from 'react-hook-form';
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 
-export function Home() {
+export function Home() {  
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
           <TaskInput 
             id="task" 
             list='task-suggestions'
-            placeholder="Dê um nome para seu projeto" 
+            placeholder="Dê um nome para seu projeto"
+            {...register('task')}      
           />
 
           <datalist id='task-suggestions'>
@@ -28,6 +39,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -41,7 +53,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
