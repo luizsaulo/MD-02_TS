fda4f6d8e69d3108768e5ae092ec368d1836f467
Saulo <saulo@blockforce.in>
2022-08-11 19:56:51 -0300

    finally was able to finish refactoring and making the application work again

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -4,9 +4,14 @@ import { CyclesContext } from "../..";
 import { CountdownContainer, Separator } from "./styles";
 
 export function Countdown() {
-  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } = useContext(CyclesContext)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
+  const { 
+    activeCycle, 
+    activeCycleId, 
+    markCurrentCycleAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+   } = useContext(CyclesContext)
+  
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   
   useEffect(() => {
@@ -21,10 +26,10 @@ export function Countdown() {
 
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
-          setAmountSecondsPassed(totalSeconds)
+          setSecondsPassed(totalSeconds)
           clearInterval(interval)        
         } else {
-          setAmountSecondsPassed(secondsDifference)
+          setSecondsPassed(secondsDifference)
         }          
       }, 1000)
     }
@@ -32,7 +37,13 @@ export function Countdown() {
     return () => {
       clearInterval(interval)
     }
-  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+  }, [
+    activeCycle, 
+    totalSeconds, 
+    activeCycleId, 
+    setSecondsPassed, 
+    markCurrentCycleAsFinished
+  ])
 
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,34 +1,12 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
-import * as zod from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 import { useContext } from 'react';
 import { CyclesContext } from '../..';
-
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5).max(60),
-})
-
-interface NewCycleFormData {
-  task: string
-  minutesAmount: number
-}
-// Uma outra forma de escrever a interface acima é da maneira abaixo:
-// type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+import { useFormContext } from 'react-hook-form';
 
 
 export function NewCycleForm() {
-  const { activeCycle } = useContext(CyclesContext)
-
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,     
-    }
-  })
+  const { activeCycle } = useContext(CyclesContext)  
+  const { register } = useFormContext()
 
     return (
         <FormContainer>
