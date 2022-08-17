3db2359140cc7da742107a8bb1ce51f13acaaa14
Saulo <saulo@blockforce.in>
2022-08-17 16:21:53 -0300

    saving an object on reducer - it has errors, but right now I´m burned out to find it

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -31,19 +31,49 @@ interface CyclesContextProviderProps {
   children: ReactNode;
 }
 
+interface CyclesState {
+  cycles: Cycle[];
+  activeCycleId: string | null;
+}
+
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
-    if (action.type == 'ADD_NEW_CYCLE') {
-      return [...state, action.payload.newCycle]
+  const [cyclesState, dispatch] = useReducer(
+    (state: CyclesState, action: any) => {
+      if (action.type === "ADD_NEW_CYCLE") {
+        return {
+          ...state,
+          cycles: [...state.cycles, action.payload.newCycle],
+          activeCycleId: action.payload.newCycle.id
+        };
+      }
+
+      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === activeCycleId) {
+              return { ...cycle, finishedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        }
+      }
+
+      return state;
+    },
+    {
+      cycles: [],
+      activeCycleId: null,
     }
+  );
 
-    return state
-  }, [])
-
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  
+  const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
@@ -53,21 +83,21 @@ export function CyclesContextProvider({
 
   function markCurrentCycleAsFinished() {
     dispatch({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      type: "MARK_CURRENT_CYCLE_AS_FINISHED",
       payload: {
         activeCycleId,
       },
-    })
+    });
 
-    // setCycles((state) =>
-    //   state.map((cycle) => {
-    //     if (cycle.id == activeCycleId) {
-    //       return { ...cycle, finishedDate: new Date() };
-    //     } else {
-    //       return cycle;
-    //     }
-    //   })
-    // );
+  //   setCycles((state) =>
+  //   state.map((cycle) => {
+  //     if (cycle.id == activeCycleId) {
+  //       return { ...cycle, interruptedDate: new Date() };
+  //     } else {
+  //       return cycle;
+  //     }
+  //   }),
+  //  ) 
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -81,37 +111,25 @@ export function CyclesContextProvider({
     };
 
     dispatch({
-      type: 'ADD_NEW_CYCLE',
+      type: "ADD_NEW_CYCLE",
       payload: {
         newCycle,
       },
-    })
+    });
 
     // setCycles((state) => [...state, newCycle]);
-    setActiveCycleId(id)
-    setAmountSecondsPassed(0)
-
+    setAmountSecondsPassed(0);
   }
 
   function interruptCurrentCycle() {
     dispatch({
-      type: 'INTERRUPT_CURRENT_CYCLE',
+      type: "INTERRUPT_CURRENT_CYCLE",
       payload: {
         activeCycleId,
       },
-    })
+    });
 
-  //   setCycles((state) =>
-  //     state.map((cycle) => {
-  //       if (cycle.id == activeCycleId) {
-  //         return { ...cycle, interruptedDate: new Date() };
-  //       } else {
-  //         return cycle;
-  //       }
-  //     })
-  //   );
-    setActiveCycleId(null);
-  }
+      
 
   return (
     <CyclesContext.Provider
