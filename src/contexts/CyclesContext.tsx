6015d1643de593c4711a33968b33231a34086477
Saulo <saulo@blockforce.in>
2022-08-25 17:26:50 -0300

    separating action types

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useState, useReducer } from "react";
-import { Cycle, cyclesReducer } from '../reducers/cycles';
+import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles';
 
 interface CreateCycleData {
   task: string;
@@ -44,7 +44,7 @@ export function CyclesContextProvider({
 
   function markCurrentCycleAsFinished() {
     dispatch({
-      type: "MARK_CURRENT_CYCLE_AS_FINISHED",
+      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
       payload: {
         activeCycleId,
       },
@@ -62,7 +62,7 @@ export function CyclesContextProvider({
     };
 
     dispatch({
-      type: "ADD_NEW_CYCLE",
+      type: ActionTypes.ADD_NEW_CYCLE,
       payload: {
         newCycle,
       },
@@ -73,7 +73,7 @@ export function CyclesContextProvider({
 
   function interruptCurrentCycle() {
     dispatch({
-      type: "INTERRUPT_CURRENT_CYCLE",
+      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
       payload: {
         activeCycleId,
       },
